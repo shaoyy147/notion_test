215cc1ea593f170baa4506eab276dedc107a63aa
FarseaSH <yy_shao@outlook.com>
2023-12-09 11:35:11 +0800

    first commit

---
+++ test.md
@@ -0,0 +1,12 @@
+![captain](https://prod-files-secure.s3.us-west-2.amazonaws.com/ba3b2760-f1a5-4a5a-b58f-c00d3288ee4b/601c3e2a-c559-4161-a027-3bdb6ae04ad7/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45HZZMZUHI%2F20231202%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20231202T024138Z&X-Amz-Expires=3600&X-Amz-Signature=994c9a58938f61bf4dc98db805b6e69db4ff320ed8187ff85bab54baf54afeea&X-Amz-SignedHeaders=host&x-id=GetObject)
+
+第一自然段，**加粗样式**，*斜体样式，*`code` 。测试，[超链接](http://www.baidu.com/)
+
+测试
+
+```python
+print("hello world")
+```
+
+> 测试
+> 第二行
---
+++ update_content.py
@@ -0,0 +1,193 @@
+# %%
+from notion_client import Client
+from typing import List, Tuple
+from textwrap import dedent
+from pprint import pprint
+from datetime import datetime
+
+import os
+
+# %%
+auth = os.environ.get("NOTION_AUTH")
+notion = Client(auth=auth)
+
+# page_blocks = notion.blocks.children.list("c98863f5-dab0-458d-abed-a294fdc1e1e9")
+# pprint(page_blocks['results'])
+
+# %%
+notion_pages = notion.databases.query("33b1d56b229e44d8b519c8b93fc1d0a3")['results']
+notion_pages
+
+
+# %%
+TIME_FORMAT = r'%Y-%m-%dT%H:%M:%S.000%z'
+class MomentPage:
+    def __init__(self, raw_page_meta_info: dict) -> None:
+        self.raw_page_meta_info: dict = raw_page_meta_info
+
+
+        self.output: List[Tuple[str, str]]= []
+
+        self.page_id: str = None,
+        self.page_name: str = None,
+        self.created_time: datetime = None,
+
+        self.author: str = None,
+        self.signature: str = None,
+
+        self.tags: List[str] = None,
+
+        self.note: str = "", 
+        self.resource: str = None
+
+
+    def parse(self):
+        notion_database_page = self.raw_page_meta_info
+
+        # -- parse meta info -- 
+        self.page_id = notion_database_page['id']
+        self.name = "".join(rich_text_part['plain_text'] for rich_text_part in notion_database_page['properties']['Name']['title'])
+
+        if notion_database_page['properties']['Date']['date']:
+            self.created_time = datetime.strptime(notion_database_page['properties']['Date']['date']['start'], TIME_FORMAT)
+        else:
+            self.created_time = datetime.strptime(notion_database_page['created_time'], TIME_FORMAT)
+
+        self.tags = [tag['name'] for tag in notion_database_page['properties']['Tags']['multi_select']]
+
+        if "Note" in notion_database_page['properties']:
+            self.note = "".join(rich_text_part['plain_text'] for rich_text_part in notion_database_page['properties']['Note']['rich_text'])
+
+        # todo
+        # author = moment_page['properties']['Author']
+        self.author = None
+
+        # todo is_top
+
+        # -- parse content blocks -0
+        page_blocks = notion.blocks.children.list(notion_database_page['id'])
+        self._parse_notion_blocks(page_blocks['results'])
+
+    
+    def to_hugo_md_page_str(self) -> str:
+        _tag_part = "\n".join(f"  - {tag}" for tag in self.tags)
+        _pictures_part = "\n".join(f"  - {text}" for text_type, text in self.output if text_type == "img")
+        yaml_part = dedent(
+            f"""
+                ---
+                top:
+                name: {self.author if self.author else ""}
+                avatar:
+
+                date: {self.created_time.strftime(r'%Y-%m-%dT%H:%M:%S%z')[:-2] + ":00"}
+
+                tags:
+                {{tag_part}}
+
+                pictures:
+                {{picture_part}}
+
+                link: {self.resource if self.resource else ""}
+                link_logo:
+                link_text: 
+
+                note: {self.note}
+                ---
+            """
+        )  \
+        .strip(" \n") \
+        .format(tag_part=_tag_part, picture_part=_pictures_part)
+
+        return yaml_part + "\n" + self._return_md_context()
+
+
+    def _return_md_context(self) -> str:
+        return "\n\n".join(text for text_type, text in self.output if text_type != "img")
+
+
+    def _parse_notion_blocks(self, blocks: List[dict]):
+        for block in blocks: self._parse_block(block)
+
+
+    def _parse_block(self, block):
+        if block['type'] == "paragraph": self._parse_paragraph(block=block)
+        if block['type'] == "image": self._parse_image(block=block)
+        if block['type'] == "code": self._parse_code(block=block)
+        if block['type'] == "quote": self._parse_quote(block=block)
+
+        # todo
+        # bulletin point
+        # embedding hyperlink -> link link_logo
+
+    def _parse_paragraph(self, block: dict):
+        md_text = self._extract_rich_text(block['paragraph']['rich_text'])
+        self.output.append(("p", md_text))
+    
+
+    def _parse_image(self, block: dict):
+        # caption = "".join(word['plain_text'] for word in block['image']['caption'])
+        # caption = self._extract_rich_text(block['image']['caption'], only_plain_text=True)
+        url = block['image']['file']['url'] if 'file' in block['image'] else block['image']['external']['url']
+        # text = f"![{caption}]({url})"
+
+        self.output.append(("img", url))
+    
+
+    def _parse_code(self, block: dict):
+        code = block['code']
+        language = code.get("language", "")
+        code_text = "".join(rich_text_part['plain_text'] for rich_text_part in code['rich_text'])
+        
+        self.output.append(("code", f"""```{language}\n{code_text}\n```"""))
+
+
+    def _parse_quote(self, block: dict):
+        rich_text_extracted = self._extract_rich_text(block['quote']['rich_text'])
+        result = "\n>\n".join("> " + line for line in rich_text_extracted.split('\n'))
+        self.output.append(("quote", result))
+
+
+    @staticmethod
+    def _extract_rich_text(rich_text: List[dict], only_plain_text=False):
+        result: List[str] = []
+        for rich_text_part in rich_text:
+            if only_plain_text:
+                result.append(rich_text_part['plain_text'])
+                continue
+
+            if 'href' in rich_text_part and rich_text_part['href']:
+                text = f"[{rich_text_part['plain_text']}]({rich_text_part['href']})"
+            else:
+                text = rich_text_part['plain_text']
+
+            if rich_text_part['annotations']['bold']: text = f"**{text}**"
+            if rich_text_part['annotations']['italic']: text = f"*{text}*"
+            if rich_text_part['annotations']['strikethrough']: text = f"~~{text}~~"
+            if rich_text_part['annotations']['code']: text = f"`{text}`"
+
+            result.append(text)
+        
+        return "".join(result)
+
+
+def write_2_markdown_files(page: MomentPage):
+    # _name = "".join(char for char in page.page_name if char.isalnum()).strip().repalce(" ", "_")
+    file_name: str = f"{page.created_time.strftime(r'%Y%m%d_%H%M')}-{page.page_id[:6]}.md"
+    with open(f"content/{file_name}", encoding="utf-8", mode="w") as f:
+        f.write(page.to_hugo_md_page_str())
+
+
+# %%
+pages = []
+for notion_page in notion_pages:
+    # todo change to concurrent parse
+    # todo update only recently modified 
+    page: MomentPage = MomentPage(raw_page_meta_info=notion_page)
+    page.parse()
+    pages.append(page)
+    # print(page.to_hugo_md_page_str())
+
+
+# %%
+for page in pages:
+    write_2_markdown_files(page)
